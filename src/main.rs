ea9087abcdf55ac3d0346d51a94dd435077b0e8a
Ryan Breen <rbreen@bamboohealth.com>
2022-06-22 12:26:36 -0400

    basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,43 @@
 use std::process::Command;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let branches = Command::new("git").arg("branch").output()?;
-    let out = String::from_utf8_lossy(&branches.stdout);
+use dialoguer::{theme::ColorfulTheme, Confirm, MultiSelect};
 
-    println!("{}", branches.status);
-    println!("{out:?}");
+fn main() -> anyhow::Result<()> {
+    let branches = Command::new("git").arg("branch").output()?;
+
+    if !branches.status.success() {
+        return Err(anyhow::anyhow!("Git branch not detected"));
+    }
+
+    let out = String::from_utf8_lossy(&branches.stdout);
+    let branches: Vec<_> = out
+        .lines()
+        .filter(|i| !i.starts_with('*'))
+        .map(|i| i.trim())
+        .collect();
+
+    let ms = MultiSelect::with_theme(&ColorfulTheme::default())
+        .items(&branches)
+        .interact()?;
+
+    let delete: Vec<_> = branches
+        .iter()
+        .enumerate()
+        .filter(|(i, _x)| ms.contains(i))
+        .map(|(_, s)| s)
+        .collect();
+
+    if Confirm::with_theme(&ColorfulTheme::default())
+        .with_prompt("Are you sure you want to continue?")
+        .interact()?
+    {
+        for n in delete {
+            let status = Command::new("git").args(["branch", "-D"]).arg(n).status()?;
+            if !status.success() {
+                return Err(anyhow::anyhow!("Unable to remove branch {n}"));
+            }
+        }
+    }
 
     return Ok(());
 }
